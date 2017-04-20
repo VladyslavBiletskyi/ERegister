27395be2feb3aa143ea717255a2f967ad6b199c9
VladyslavBiletskyi <vladyslav.biletskyi@nure.ua>
2017-04-20 18:21:51 +0300

    Добавьте файлы проекта.

---
+++ ERegister/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ERegister.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ERegister/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ERegister.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
